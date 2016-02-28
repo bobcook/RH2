5a08d78b280f9285b0215bc5fccde65d97ef1f50
Bob Cook <bobcook@sunrem.com>
2016-02-27 20:45:38 -0700

    still applying date sorts

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -68,7 +68,7 @@ class MembersController < ApplicationController
     @members = Member.all
     @adults = @members.find_all { |p| p.birth_date < (Date.today - 7500) }
     parts = @adults.partition { |o| o.prayers.blank? }
-    @prayable = parts.last.sort_by { |p| p.prayers.last } + parts.first
+    @prayable = parts.last.sort_by { |p| p.prayers.first } + parts.first
     return @prayable
   end
 
