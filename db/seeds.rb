0329338a2c126337a3f51c8f2f29f80e06966f4e
Bob Cook <bobcook@sunrem.com>
2017-02-13 07:35:47 -0700

    Fixed seed to set current

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'csv'
 
+Member.update_all(current: false)
+
 csv_text = File.read('members.csv')
 csv = CSV.parse(csv_text, headers: true)
 csv.each do |row|
@@ -7,21 +9,13 @@ csv.each do |row|
   member_record['birth_date'] = member_record['birth_date'].to_date
   db_record = Member.find_by(name: member_record['name'])
   if db_record.nil?
-    Member.create(member_record)
+    Member.create(member_record, current: true)
   else
     db_record.update(name: member_record['name'],
                      gender: member_record['gender'],
                      birth_date: member_record['birth_date'],
                      phone_number: member_record['phone_number'],
-                     email: member_record['email'])
-  end
-end
-
-@members = Member.all
-@members.each do |x|
-  if x.updated_at.to_date < Date.today
-    x.update(current: false)
-  else
-    x.update(current: true)
+                     email: member_record['email'],
+                     current: true)
   end
 end
