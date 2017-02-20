ebb4590dd81912377ae28248dc23043080c86c86
Bob Cook <bobcook@sunrem.com>
2017-02-20 15:12:38 -0700

    Adds exempt and limits speakable and prayable

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,14 @@ csv.each do |row|
   member_record['birth_date'] = member_record['birth_date'].to_date
   db_record = Member.find_by(name: member_record['name'])
   if db_record.nil?
-    Member.create(member_record, current: true)
+    Member.create(member_record, current: true, exempt: false)
   else
     db_record.update(name: member_record['name'],
                      gender: member_record['gender'],
                      birth_date: member_record['birth_date'],
                      phone_number: member_record['phone_number'],
                      email: member_record['email'],
-                     current: true)
+                     current: true,
+                     exempt: false)
   end
 end
